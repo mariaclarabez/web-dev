17441b38cda0d7cf62fd2dfe5289c23303fddfb8
mariaclarabez <mariaclarabez6@gmail.com>
2022-03-20 14:34:05 -0400

    a6 React.js sp22

--- src/components/tuiter/index.js
+++ src/components/tuiter/index.js
@@ -2,14 +2,13 @@ import React from "react";
 import NavigationSidebar from "./NavigationSidebar/index.js";
 import WhoToFollowList from "./WhoToFollowList/index.js";
 import PostSummaryList from "./PostSummaryList/index.js";
+import ExploreScreen from "./ExploreScreen/ExploreScreen.js";
 
 
 const Tuiter = () => {
     return(
         <>
-            <PostSummaryList/>
-            <NavigationSidebar active="home"/>
-            <WhoToFollowList/>
+            <ExploreScreen/>
         </>
 
     )

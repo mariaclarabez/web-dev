d1756fe0caaa2e18e6eb7a581c8602ebb4c19f9a
mariaclarabez <mariaclarabez6@gmail.com>
2022-03-28 19:44:54 -0400

    a7 Redux sp22

--- src/components/tuiter/ProfileScreen/EditProfile.js
+++ src/components/tuiter/ProfileScreen/EditProfile.js
@@ -1,5 +1,5 @@
 import {useDispatch, useSelector} from "react-redux";
-import {useState} from "react";
+import React, {useState} from "react";
 
 const EditProfile = (props) => {
     const {editProfileOn, setEditProfileOn} = props;
@@ -27,9 +27,26 @@ const EditProfile = (props) => {
 
     return (
         <div>
-            <div class="container">
-                <div class="row">
-                    <div class="col"><img className="wd-banner" src={owner.banner}></img></div>
+            <div className="container wd-font">
+                <div className="row wd-top-container">
+                    <div className="row wd-top-container">
+                        <span>
+                            <i onClick={() => setEditProfileOn(false)}
+                               className="fas fa-times wd-gray-text wd-cancel-edit-profile"></i>
+                        <b className="wd-gray-text">Edit Profile</b>
+                            <span className="wd-save-changes"><button
+
+                                className="btn rounded-pill wd-save-changes-btn {
+"
+                                onClick={() => editClickHandler()}>
+                                    <b>Save</b>
+                                </button></span>
+                        </span>
+                    </div>
+                    <div className="row">
+                    <div class="col">
+
+                        <img className="wd-banner" src={owner.banner}></img></div>
                     <div className="container">
                         <div className="row">
                             <div className="col-9">
@@ -37,42 +54,44 @@ const EditProfile = (props) => {
                       <img className="rounded-circle over" src={owner.avatarIcon}/>
           </span>
 
-                                <button
-                                    className="btn btn-success btn-block rounded-pill wd-edit-buttons {
-"
-                                    onClick={() => editClickHandler()}>
-                                    Save Changes
-                                </button>
-                                <button
-                                    className="btn btn-danger rounded-pill wd-edit-buttons {
-"
-                                    onClick={() => setEditProfileOn(false)}>
-                                    Cancel Changes
-                                </button>
-
                             </div>
 
                         </div>
                     </div>
                 </div>
-                <div className="row">
-                    <h5>{owner.firstName} {owner.lastName}</h5>
-                    <input type="email" className="form-control wd-form" id="exampleFormControlInput1"
-                           defaultValue={userName} onChange={e => setUserName(e.target.value)}></input>
-
-                    <input type="email" className="form-control wd-form" id="exampleFormControlInput1"
-                           defaultValue={userWeb} onChange={e => setUserWeb(e.target.value)}></input>
-
-                    <textarea className="form-control wd-formarg " rows="3" defaultValue={userBio}
-                              onChange={e => setUserBio(e.target.value)}></textarea>
-                    <span className="wd-user-handle"><input type="email" className="form-control wd-form"
-                                                            id="exampleFormControlInput1" defaultValue={userLoc}
-                                                            onChange={e => setUserLoc(e.target.value)}></input>
-             <input type="email" className="form-control" id="exampleFormControlInput1" defaultValue={userDOB}
-                    onChange={e => setUserDOB(e.target.value)}></input>  | {owner.dateJoined} </span>
                 </div>
                 <div className="row">
-                    <h6>Following {owner.followingCount} | Followers {owner.followersCount}</h6>
+                    <span className="wd-user-name"> <b>{owner.firstName} {owner.lastName} </b></span>
+                    <input type="email" className="form-control wd-form wd-user-handle" id="exampleFormControlInput1"
+                           defaultValue={userName} onChange={e => setUserName(e.target.value)}></input>
+
+                    <textarea className="form-control wd-formarg wd-user-handle" rows="3" defaultValue={userBio}
+                              onChange={e => setUserBio(e.target.value)}></textarea>
+
+                    <input type="email" className="form-control wd-form wd-user-handle" id="exampleFormControlInput1"
+                           defaultValue={userWeb} onChange={e => setUserWeb(e.target.value)}></input>
+
+
+                    <input type="email" className="form-control wd-form wd-user-handle"
+                                                            id="exampleFormControlInput1" defaultValue={userLoc}
+                                                            onChange={e => setUserLoc(e.target.value)}></input>
+             <input type="email" className="form-control wd-user-handle" id="exampleFormControlInput1" defaultValue={userDOB}
+                    onChange={e => setUserDOB(e.target.value)}></input>
+                    <span className="wd-user-handle">
+                        <span className="wd-profile-item">
+                            <i className="fas fa-calendar"></i>
+                            <b> Joined {owner.dateJoined}</b></span>
+                    </span>
+                </div>
+                <div className="row">
+                        <span className="wd-follow">
+                            <span className="wd-follow-item">
+                                <span className="wd-follow-number"><b>{owner.followingCount} </b></span>
+                                <b>Following</b></span>
+                            <span className="wd-follow-item">
+                                <span className="wd-follow-number"><b>{owner.followersCount} </b></span>
+                                <b>Followers</b></span>
+                        </span>
                 </div>
 
             </div>
--- src/components/tuiter/ProfileScreen/Profile.js
+++ src/components/tuiter/ProfileScreen/Profile.js
@@ -16,6 +16,11 @@ const Profile = () => {
 
                 <div>
                 <div className="container wd-font">
+                    <div className="row wd-top-container">
+                        <span><a href="/tuiter/"><i className="fas fa-arrow-left wd-back-button"></i></a>
+                        <b>{owner.firstName} {owner.lastName}</b></span>
+                        <div className="wd-tweets-num"><b>5,000 Tweets</b></div>
+                    </div>
                     <div className="row">
                         <div className="col"><img className="wd-banner" src={owner.banner}/></div>
                         <div className="container">
@@ -37,18 +42,32 @@ const Profile = () => {
                         </div>
                     </div>
                     <div className="row">
-                        <span className="wd-user-name"> <b>{owner.firstName} {owner.lastName} | </b>
-                                                <span className="wd-user-handle"> @{owner.handle}</span>
+                        <span className="wd-user-name"> <b>{owner.firstName} {owner.lastName} </b>
                         </span>
-                        <span className="wd-bio"><h6>{owner.bio}
-                        </h6></span>
+                        <span className="wd-user-handle"> <b>@{owner.handle}</b></span>
+
+                        <span className="wd-bio">{owner.bio}
+                        </span>
                         <span className="wd-website">{owner.website}</span>
-                        <span
-                            className="wd-profile-details">{owner.location} | {owner.dateOfBirth} | {owner.dateJoined} </span>
+                        <span className="wd-profile-details"><b>
+                            <i className="fas fa-map-pin"></i>
+                            <span className="wd-profile-item"> {owner.location}</span>
+                            <i className="fas fa-birthday-cake"></i>
+                            <span className="wd-profile-item"> Born {owner.dateOfBirth}</span>
+                            <i className="fas fa-calendar"></i>
+                            <span className="wd-profile-item"> Joined {owner.dateJoined}</span></b>
+                           </span>
 
                     </div>
                     <div className="row">
-                        <span className="wd-profile-details">Following {owner.followingCount} | Followers {owner.followersCount}</span>
+                        <span className="wd-follow">
+                            <span className="wd-follow-item">
+                                <span className="wd-follow-number"><b>{owner.followingCount} </b></span>
+                                <b>Following</b></span>
+                            <span className="wd-follow-item">
+                                <span className="wd-follow-number"><b>{owner.followersCount} </b></span>
+                                <b>Followers</b></span>
+                        </span>
                     </div>
                 </div>
 

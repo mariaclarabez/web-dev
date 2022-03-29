97573561b3c5dfcd1e1acb20922d498b499989f1
mariaclarabez <mariaclarabez6@gmail.com>
2022-03-28 22:25:10 -0400

    a7 Redux sp22

---
+++ public/tuiter/HomeScreen/HomeComponent.js
@@ -0,0 +1,18 @@
+import PostList from "../PostList/PostList.js";
+
+const HomeComponent = () => {
+    return (`
+            <div class="row">
+                <div class="wd-header">
+                    <div class="wd-search-area">
+                        <i class="fas fa-search wd-mag-image"></i>
+                        <input class="wd-search-bar" type="text" placeholder="Search Tuiter"/>
+                    </div>
+                    <a href="settings.html"><i class="fa fa-cog wd-settings-icon"></i></a>
+                </div>
+            </div>
+            ${PostList()}
+    `);
+}
+
+export default HomeComponent;
